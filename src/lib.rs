09c63424761539213b5463021b9ddac556a8271e
Raphaël Deschênes <raphdf@hotmail.com>
2024-05-17 11:56:43 -0400

    Prepared for release

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-use std::fs;
-use std::error::Error;
+//! # minigrep lib
+//!
+//! A library for my minigrep project
 
+use std::error::Error;
+use std::fs;
+
+/// Runs the search engine,
+/// reads the file specified in the filename argument and
+/// outputs the lines of the file that contain the query
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents =fs::read_to_string(config.filename)?;
+    let contents = fs::read_to_string(config.filename)?;
 
     for line in search(&config.query, &contents) {
         println!("{}", line)
@@ -11,10 +18,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+/// Contains the fields for the search engine/arguments
 pub struct Config {
     pub query: String,
     pub filename: String,
-
 }
 
 impl Config {
@@ -25,10 +32,11 @@ impl Config {
         let query: String = args[1].clone();
         let filename: String = args[2].clone();
 
-        Ok(Config {query, filename})
+        Ok(Config { query, filename })
     }
 }
 
+/// Searches the query in a file
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args:Vec<String> = env::args().collect();
-    
+    let args: Vec<String> = env::args().collect();
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments : {}", err);
         process::exit(1)
