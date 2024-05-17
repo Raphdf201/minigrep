1bcae9c5fdd220f8aaacedccc63710c0bd43a11c
Raphaël Deschênes <raphdf@hotmail.com>
2024-05-17 09:57:02 -0400

    Implemented Config

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args:Vec<String> = env::args().collect();
     
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -21,9 +21,11 @@ struct Config {
 
 }
 
-fn parse_config(args: &[String]) -> Config {
+impl Config {
+    fn new(args: &[String]) -> Config {
     let query: String = args[1].clone();
     let filename: String = args[2].clone();
 
     Config {query, filename}
+    }
 }
