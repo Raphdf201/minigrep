0142880d98dd1028d9a950be0c03b971ce718170
Raphaël Deschênes <raphdf@hotmail.com>
2024-05-17 11:57:25 -0400

    Fixed import

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use searcher_txt::run;
+use searcher_txt::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,7 +15,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         println!("Application error : {}", e);
         process::exit(1);
     }
