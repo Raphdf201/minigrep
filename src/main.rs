e23dd470cee2df0278ebef3d5c5eb51907b27878
Raphaël Deschênes <raphdf@hotmail.com>
2024-05-17 09:53:16 -0400

    Changed project structure

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,27 @@ use std::fs;
 
 fn main() {
     let args:Vec<String> = env::args().collect();
-
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
     
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = parse_config(&args);
 
-    let contents =fs::read_to_string(filename)
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents =fs::read_to_string(config.filename)
         .expect("Cannot read the file. Ensure the title and the file extension are ok");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query: String = args[1].clone();
+    let filename: String = args[2].clone();
+
+    Config {query, filename}
 }
